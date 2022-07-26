20cbf7229325f9aa6e76444c7f8b82d7e9ee8dd0
Anthony N. Rivera Vega <arivera@leal.co>
2022-07-25 23:27:44 -0500

    initial

---
+++ apiServices/Controllers/Services.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using apiServices.Model;
+
+namespace apiServices.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class Services : ControllerBase
+    {
+
+        private readonly ILogger<Services> _logger;
+
+        private static readonly string[] Items = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        public Services(ILogger<Services> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "/Api1")]
+        public IEnumerable<Api1> GetApi1()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Api1
+            {
+                id = index,
+                Contact_Address = Items[index].ToString(),
+                Warehouse_Address = Items[index].ToString(),
+                dimension = Items[Random.Shared.Next(Items.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPut(Name = "/Api2")]
+        public IEnumerable<Api2> GetApi2()
+        {
+            return Enumerable.Range(1, 5).Select(i => new Api2
+            {
+                id = i,
+                Consignor = Items[i].ToString(),
+                Cartons = Items[Random.Shared.Next(Items.Length)],
+                Consignee = Items[i].ToString()
+            })
+                .ToArray();
+        }
+
+        [HttpGet(Name = "/Api3")]
+        public IEnumerable<Api3> GetApi3()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Api3
+            {
+                id = index,
+                Destination = Items[index].ToString(),
+                Source = Items[index].ToString(),
+                Package = Items[Random.Shared.Next(Items.Length)]
+            })
+                .ToArray();
+        }
+    }
+}
+
---
+++ apiServices/Model/General.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace apiServices.Model
+{
+
+    public class Api1
+    {
+        public int id { get; set; }
+        public string? Contact_Address { get; set; }
+        public string? Warehouse_Address { get; set; }
+        public string? dimension { get; set; }
+    }
+
+
+
+    public class Api2
+    {
+        public int id { get; set; }
+        public string? Consignee { get; set; }
+        public string? Consignor { get; set; }
+        public string? Cartons { get; set; }
+    }
+
+    public class Api3
+    {
+        public int id { get; set; }
+        public string? Source { get; set; }
+        public string? Destination { get; set; }
+        public string? Package { get; set; }
+    }
+}
+
